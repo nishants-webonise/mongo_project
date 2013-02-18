08f4029cfd25f4de7f5c0990ca0d99c9ef06ee1f
Nishant Samel <nishants@weboniselab.com>
2013-02-18 14:36:23 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoProject::Application.config.secret_token = 'e5dc64a38c3b5226ef35af3223aa67ef753b0bafcf42a5b551dd221db742f7cd36401fb7c57054e1cfb6fd368d03029b35645ddb344e64305e31047ff2b2610f'
